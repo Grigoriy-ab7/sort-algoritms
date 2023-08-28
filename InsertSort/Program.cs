28b5c4a1578cf40a9b655a928d7a04f7f3719e35
Grigoriy_Ab <ab.grigoriy7@gmail.com>
2023-08-28 22:28:33 +0600

    add insert sortt

--- InsertSort/Program.cs
+++ InsertSort/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] arr = {1, 3, 2, 5, 7, 10, 9, 6, 8};
+
+for (int i = 1; i < arr.Length; i++)
+{
+    int temp = arr[i];
+    int j = i;
+    while(j > 0 && arr[j-1] > temp)
+    {
+        arr[j] = arr[j-1];
+        j--;
+    }
+    arr[j] = temp;
+}
